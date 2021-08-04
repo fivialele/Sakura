d37905a21986134b7b005d59f38f54bf15b0d949
Fivia <fivylele@yahoo.com>
2021-08-04 15:56:44 +0300

    Add project files.

---
+++ LuisHelper/LuisHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.AI.Luis;
+using Microsoft.Bot.Configuration;
+
+namespace Microsoft.BotBuilderSamples.LuisHelper
+{
+    public class LuisHelper : IRecognizer
+    {
+        private readonly LuisRecognizer _luisRecognizer;
+
+        [Obsolete]
+        public LuisHelper()
+        {
+
+            var service = new LuisService()
+            {
+                AppId = "768c2fe6-b7b7-452e-9898-1fe553a2ce82",
+                SubscriptionKey = "c947d92f0691440684afc9357a91aa32",
+                Region = "westeurope",
+                Version = ""
+            };
+
+            var app = new LuisApplication(service);
+            var regOptions = new LuisRecognizerOptionsV2(app)
+            {
+                IncludeAPIResults = true,
+                PredictionOptions = new LuisPredictionOptions()
+                {
+                    IncludeAllIntents = true,
+                    IncludeInstanceData = true
+                }
+            };
+
+            _luisRecognizer = new LuisRecognizer(regOptions);
+
+        }
+
+        public async Task<RecognizerResult> RecognizeAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            return await _luisRecognizer.RecognizeAsync(turnContext, cancellationToken);
+        }
+
+        public async Task<T> RecognizeAsync<T>(ITurnContext turnContext, CancellationToken cancellationToken) where T : IRecognizerConvert, new()
+        {
+            return await _luisRecognizer.RecognizeAsync<T>(turnContext, cancellationToken);
+        }
+    }
+}
---
+++ LuisSample.cs
@@ -0,0 +1,108 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Luis;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace LuisSample
+{
+    public class MyLuisSample : ActivityHandler
+    {
+        private Microsoft.BotBuilderSamples.LuisHelper.LuisHelper _luisHelper;
+        public MyLuisSample(Microsoft.BotBuilderSamples.LuisHelper.LuisHelper luisHelper)
+        {
+            _luisHelper = luisHelper;
+        }
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text($":)"), cancellationToken);
+                }
+            }
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var result = await _luisHelper.RecognizeAsync<LuisForSakuraFirstTry>(turnContext, cancellationToken);
+            var topIntent = result.TopIntent().intent;
+
+            switch (topIntent)
+            {
+                case LuisForSakuraFirstTry.Intent.None:
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"None"), cancellationToken);
+                    break;
+                case LuisForSakuraFirstTry.Intent.Feel_Amused:
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Amused"), cancellationToken);
+                    break;
+                case LuisForSakuraFirstTry.Intent.Feel_Angry:
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Angry"), cancellationToken);
+                    break;
+                case LuisForSakuraFirstTry.Intent.Feel_Anxious:
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Anxious"), cancellationToken);
+                    break;
+                case LuisForSakuraFirstTry.Intent.Feel_Bored:
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Bored"), cancellationToken);
+                    break;
+                case LuisForSakuraFirstTry.Intent.Feel_Confident:
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Confident"), cancellationToken);
+                    break;
+                case LuisForSakuraFirstTry.Intent.Feel_Disappointed:
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Disappointed"), cancellationToken);
+                    break;
+                case LuisForSakuraFirstTry.Intent.Feel_Embarrassed:
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Embarrassed"), cancellationToken);
+                    break;
+                case LuisForSakuraFirstTry.Intent.Feel_Flirty:
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Flirty"), cancellationToken);
+                    break;
+                case LuisForSakuraFirstTry.Intent.Feel_Frightened:
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Frightened"), cancellationToken);
+                    break;
+                case LuisForSakuraFirstTry.Intent.Feel_Guilty:
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Guilty"), cancellationToken);
+                    break;
+                case LuisForSakuraFirstTry.Intent.Feel_Happy:
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Happy"), cancellationToken);
+                    break;
+                case LuisForSakuraFirstTry.Intent.Feel_Hungry:
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Hungry"), cancellationToken);
+                    break;
+                case LuisForSakuraFirstTry.Intent.Feel_Insecure:
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Insecure"), cancellationToken);
+                    break;
+                case LuisForSakuraFirstTry.Intent.Feel_Jealous:
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Jealous"), cancellationToken);
+                    break;
+                case LuisForSakuraFirstTry.Intent.Feel_Sad:
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Sad"), cancellationToken);
+                    break;
+                case LuisForSakuraFirstTry.Intent.Feel_Safe:
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Safe"), cancellationToken);
+                    break;
+                case LuisForSakuraFirstTry.Intent.Feel_Sick:
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Sick"), cancellationToken);
+                    break;
+                case LuisForSakuraFirstTry.Intent.Feel_Stressed:
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Stressed"), cancellationToken);
+                    break;
+                case LuisForSakuraFirstTry.Intent.Feel_Thirsty:
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Thirsty"), cancellationToken);
+                    break;
+                case LuisForSakuraFirstTry.Intent.Feel_Tired:
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Tired"), cancellationToken);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+        }
+    }
+}
